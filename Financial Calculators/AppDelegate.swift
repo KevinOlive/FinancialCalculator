7926c4762be14244404086e20986885c111e66bc
Kevin Olive <genius@olivemanor.com>
2019-04-08 13:56:14 -0500

    Initial Commit

---
+++ Financial Calculators/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Financial Calculators
+//
+//  Created by Kevin Olive on 4/8/19.
+//  Copyright © 2019 Kevin Olive. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
